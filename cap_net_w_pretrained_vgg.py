43de5c99b657b90fca9ebc8e3825f45344955426
lavenderrz <32234273+lavenderrz@users.noreply.github.com>
2018-03-03 02:32:12 -0500

    Update cap_net_w_pretrained_vgg.py

--- cap_net_w_pretrained_vgg.py
+++ cap_net_w_pretrained_vgg.py
@@ -7,7 +7,35 @@ import numpy as np
 
 #load vgg16 with pretrained weights
 import torchvision.models as models
-vgg16 = models.vgg16(pretrained=True)
+our_vgg = models.vgg16(pretrained=True)
 
+#cuda variables
+use_gpu = torch.cuda.is_available()
 
+# Input dimensions of VGG16 input image
+VGG_IMG_DIM = 224
+
+# Recurrent size must be same as last hidden layer off VGG16
+RNN_HIDDEN_SIZE = 4096
+
+# Dimension of word embeddings
+WORDVEC_SIZE = 300
+
+# Assume a limited language model consisting of this many words
+VOCABULARY_SIZE = 6000
+
+our_vgg.classifier = nn.Sequential(
+      nn.Linear(512 * 7 * 7, 4096),
+      nn.ReLU(True),
+      nn.Dropout(),
+      nn.Linear(4096, RNN_HIDDEN_SIZE),
+      nn.ReLU(True),
+      nn.Dropout(),
+      nn.Linear(RNN_HIDDEN_SIZE, num_classes),
+    )
+
+if use_gpu:
+  our_vgg = our_vgg.cuda()
+
+  
 
